1d3fb8416eedb393476b021a75a7d9066c90e0cb
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-12 00:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = batteryBatches.reduce((total, element) => element + total)
+let totalBatteries = batteryBatches.reduce((total, element) => element + total);
 
 // which is the sum of all of the battery amounts in the batteryBatches array. Naturally, use reduce() for this!
